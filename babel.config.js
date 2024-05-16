c158c0c95d366ffad9d83a7f00c3c02970aad8a1
unknown <ajayp@ithena.ai>
2024-05-16 18:38:31 +0530

    fix worklet issue

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  env: {
-    production: {
-      plugins: [
-        'react-native-paper/babel',
-        '@babel/plugin-proposal-export-namespace-from',
-        'react-native-reanimated/plugin',
-      ],
-    },
-  },
+  plugins: [
+    'react-native-paper/babel',
+    '@babel/plugin-proposal-export-namespace-from',
+    'react-native-reanimated/plugin',
+  ],
 };
