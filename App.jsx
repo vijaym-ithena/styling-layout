1f8a84a7f18c79e1301e7d9a5340f27103c86005
unknown <ajayp@ithena.ai>
2024-05-16 18:50:18 +0530

    chore(app): code formatting perfomed

--- App.jsx
+++ App.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { PaperProvider } from 'react-native-paper';
-import { LoginScreen, OtpScreen, PhoneLoginScreen, SignupScreen } from './screens';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {PaperProvider} from 'react-native-paper';
+import {
+  LoginScreen,
+  OtpScreen,
+  PhoneLoginScreen,
+  SignupScreen,
+} from './screens';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
-// const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <GestureHandlerRootView>
         <PaperProvider>
-            <Stack.Navigator
-                initialRouteName='login'
-                screenOptions={{
-                    headerShown: false
-                }}
-            >
-                <Stack.Screen name='login' component={LoginScreen} />
-                <Stack.Screen name='signup' component={SignupScreen} />
-                <Stack.Screen name='phonelogin' component={PhoneLoginScreen} />
-                <Stack.Screen name='otp' component={OtpScreen} />
-            </Stack.Navigator>
+          <Stack.Navigator
+            initialRouteName="login"
+            screenOptions={{
+              headerShown: false,
+            }}>
+            <Stack.Screen name="login" component={LoginScreen} />
+            <Stack.Screen name="signup" component={SignupScreen} />
+            <Stack.Screen name="phonelogin" component={PhoneLoginScreen} />
+            <Stack.Screen name="otp" component={OtpScreen} />
+          </Stack.Navigator>
         </PaperProvider>
+      </GestureHandlerRootView>
     </NavigationContainer>
   );
 }
--- screens/PhoneLoginScreen.jsx
+++ screens/PhoneLoginScreen.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button, TextInput, TouchableRipple } from 'react-native-paper';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Button, TextInput, TouchableRipple} from 'react-native-paper';
 
-export default function PhoneLoginScreen({ navigation }) {
+export default function PhoneLoginScreen({navigation}) {
   const [countryCode, setCountryCode] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
 
@@ -12,11 +11,13 @@ export default function PhoneLoginScreen({ navigation }) {
       <View>
         <Button
           icon={'close'}
-          contentStyle={{ flexDirection: 'row-reverse' }}
-          style={{ alignSelf: 'flex-end', padding: 0 }}
+          contentStyle={{flexDirection: 'row-reverse'}}
+          style={{alignSelf: 'flex-end', padding: 0}}
           onPress={() => console.log('PhoneLoginScreen closed!')}
         />
-        <Text style={styles.heading}>Quick Sign-up / Sign-in with your phone</Text>
+        <Text style={styles.heading}>
+          Quick Sign-up / Sign-in with your phone
+        </Text>
         <View style={styles.numberContainer}>
           <TextInput
             label={'Country Code'}
@@ -25,8 +26,8 @@ export default function PhoneLoginScreen({ navigation }) {
             activeUnderlineColor="#0076ba"
             cursorColor="#0076ba"
             value={countryCode}
-            style={[styles.input, { width: '20%' }]}
-            contentStyle={{ paddingLeft: 0 }}
+            style={[styles.input, {width: '20%'}]}
+            contentStyle={{paddingLeft: 0}}
             onChangeText={newCode => setCountryCode(newCode)}
           />
           <TextInput
@@ -36,8 +37,8 @@ export default function PhoneLoginScreen({ navigation }) {
             activeUnderlineColor="#0076ba"
             cursorColor="#0076ba"
             value={phoneNumber}
-            style={[styles.input, { width: '80%' }]}
-            contentStyle={{ paddingLeft: 0 }}
+            style={[styles.input, {width: '80%'}]}
+            contentStyle={{paddingLeft: 0}}
             onChangeText={newNumber => setPhoneNumber(newNumber)}
           />
         </View>
@@ -45,10 +46,16 @@ export default function PhoneLoginScreen({ navigation }) {
       <View>
         <TouchableRipple
           rippleColor={'#FFEAE0'}
-          style={{ marginVertical: 20, padding: 5, display: 'flex', alignSelf: 'flex-end' }}
-          onPress={() => navigation.navigate('login')}
-        >
-          <Text style={{ color: '#ff6a28', fontWeight: '600', fontSize: 12 }}>Try with Email address</Text>
+          style={{
+            marginVertical: 20,
+            padding: 5,
+            display: 'flex',
+            alignSelf: 'flex-end',
+          }}
+          onPress={() => navigation.navigate('login')}>
+          <Text style={{color: '#ff6a28', fontWeight: '600', fontSize: 12}}>
+            Try with Email address
+          </Text>
         </TouchableRipple>
         <Button
           mode="contained"
@@ -58,24 +65,36 @@ export default function PhoneLoginScreen({ navigation }) {
           // #d9e7f3 - original ripple color
           rippleColor="#D0EEFF"
           style={styles.button}
-          onPress={() => navigation.navigate('otp')}
-        >Send OTP</Button>
-        <View style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', alignSelf: 'flex-start' }}>
-          <Text style={{ fontSize: 10 }}>By continuing, you agree to ITHENA's </Text>
+          onPress={() => navigation.navigate('otp')}>
+          Send OTP
+        </Button>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            alignSelf: 'flex-start',
+          }}>
+          <Text style={{fontSize: 10}}>
+            By continuing, you agree to ITHENA's{' '}
+          </Text>
           <TouchableRipple
             rippleColor={'#FFEAE0'}
-            style={{ display: 'flex', alignSelf: 'flex-start' }}
-            onPress={() => console.log('TnC pressed!')}
-          >
-            <Text style={{ color: '#ff6a28', fontWeight: '600', fontSize: 10 }}>Terms & Conditions</Text>
+            style={{display: 'flex', alignSelf: 'flex-start'}}
+            onPress={() => console.log('TnC pressed!')}>
+            <Text style={{color: '#ff6a28', fontWeight: '600', fontSize: 10}}>
+              Terms & Conditions
+            </Text>
           </TouchableRipple>
-          <Text style={{ fontSize: 10 }}> and </Text>
+          <Text style={{fontSize: 10}}> and </Text>
           <TouchableRipple
             rippleColor={'#FFEAE0'}
-            style={{ display: 'flex', alignSelf: 'flex-start' }}
-            onPress={() => console.log('Privacy policy pressed!')}
-          >
-            <Text style={{ color: '#ff6a28', fontWeight: '600', fontSize: 10 }}>Privacy Policy</Text>
+            style={{display: 'flex', alignSelf: 'flex-start'}}
+            onPress={() => console.log('Privacy policy pressed!')}>
+            <Text style={{color: '#ff6a28', fontWeight: '600', fontSize: 10}}>
+              Privacy Policy
+            </Text>
           </TouchableRipple>
         </View>
       </View>
@@ -89,24 +108,24 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 20,
-    paddingVertical: 30
+    paddingVertical: 30,
   },
   heading: {
     marginVertical: 25,
     fontSize: 18,
     color: '#808080',
     width: 'auto',
-    textAlign: 'left'
+    textAlign: 'left',
   },
   button: {
     borderRadius: 10,
     width: 'auto',
-    marginBottom: 10
+    marginBottom: 10,
   },
   input: {
     textAlign: 'left',
     backgroundColor: 'transparent',
-    fontSize: 14
+    fontSize: 14,
   },
   numberContainer: {
     display: 'flex',
@@ -114,6 +133,6 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     gap: 5,
-    width: '100%'
-  }
+    width: '100%',
+  },
 });
